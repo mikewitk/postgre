843db4c70a6e1a5d36580486286bfcd4f739627b
Michael <mike.witk@gmail.com>
2019-04-23 18:56:08 +0000

    Search function with Knex implemented

--- knex_people.js
+++ knex_people.js
@@ -1,25 +1,32 @@
 
 module.exports = (function() {
 
-const options = {
-    client: 'pg',
-    connection: {
-        host: '127.0.0.1',
-        user: 'vagrant',
-        password: 'vagrant',
-        database: 'test_db'
-    }
-}
 
-const knex = require('knex')('options');
+var knex = require('knex')({
+  client: 'pg',
+  version: '9.5',
+  connection: {
+    host : '127.0.0.1',
+    user : 'vagrant',
+    password : 'vagrant',
+    database : 'test_db'
+  },
+    pool: {min: 0}
+});
 
+// const knex = require('knex')('options');
+console.log("I am connected...I guess")
 
 
   function findPeople (name, callback) {
 
-    knex.select('first_name').from('famous_people')
-        .where('first_name', '=', name)
-
+    knex.from('famous_people')
+        .select('first_name', 'last_name','birthdate')
+        .where('first_name', name)
+        .then((rows) => {
+          callback(rows)
+          knex.destroy();
+        })
 
   }
 
